1a99a4b0fe591219d8230d9777cd6641b99d2d72
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-03-16 21:45:18 +0100

    remove player on socket close

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ server.on('connection', (socket, request) => {
   });
 
   socket.on('close', () => {
+    if (game) {
+      game.removePlayer(socket);
+      broadcast({
+        type: 'PLAYERS',
+        players: Array.from(game.players.values()),
+      });
+    }
+
     manager.leave(room, socket);
   });
 });
